8620d5b29b89daa1fe63e7f4558b208d424872c1
Alex Goia <alexgoiadev@gmail.com>
2020-08-16 12:54:47 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Theme customizations
+ *
+ * @package  BaseThemeGenesis
+ * @author  Álex Goia
+ * @link  https://github.com/ibadrehman/custom
+ * @copyright  Copyright (c) 2020, Álex Goia
+ * @license  GPL-2.0+
+ */
+
+load_child_theme_textdomain( 'custom' );
+
+function custom_setup(){}
+
+
+?>
