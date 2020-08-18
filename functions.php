385f21ae3c2b5745b0f28b50e2c6c2992359a21e
Alex Goia <alexgoiadev@gmail.com>
2020-08-18 10:50:46 +0200

    Added base info

--- functions.php
+++ functions.php
@@ -1,17 +1,16 @@
 <?php
-/**
- * Theme customizations
- *
- * @package  BaseThemeGenesis
- * @author  Álex Goia
- * @link  https://github.com/ibadrehman/custom
- * @copyright  Copyright (c) 2020, Álex Goia
- * @license  GPL-2.0+
- */
 
-load_child_theme_textdomain( 'custom' );
+include_once( get_template_directory() . '/lib/init.php' );
 
-function custom_setup(){}
+define( 'Child_Theme_Name', __( 'Genesis Child', 'BaseGenesisTheme' ) );
 
+//* We tell the web address of our child theme (More info & demo)
+define( 'Child_Theme_Url', ' https://github.com/AlexGoiaDev/basegenesistheme.git' );
+//* We tell the version of our child theme
+define( 'Child_Theme_Version', '1.0' );
 
-?>
+//* Add HTML5 markup structure from Genesis
+add_theme_support( 'html5' );
+
+//* Add HTML5 responsive recognition
+add_theme_support( 'genesis-responsive-viewport' );
