25af9d228d55f77f1c36eb8ff40afa47e89cd357
Alex Goia <alexgoiadev@gmail.com>
2020-08-18 10:51:42 +0200

    Basic configu

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 define( 'Child_Theme_Name', __( 'Genesis Child', 'BaseGenesisTheme' ) );
 
 //* We tell the web address of our child theme (More info & demo)
-define( 'Child_Theme_Url', ' https://github.com/AlexGoiaDev/basegenesistheme.git' );
+define( 'Child_Theme_Url', 'https://github.com/AlexGoiaDev/basegenesistheme.git' );
 //* We tell the version of our child theme
 define( 'Child_Theme_Version', '1.0' );
 
