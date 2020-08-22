f06297729626e5f9edf4b4b45e60e1b3b499c389
Alex Goia <alexgoiadev@gmail.com>
2020-08-22 12:41:22 +0200

    Removed footer

--- functions.php
+++ functions.php
@@ -1,16 +1,30 @@
 <?php
 
-include_once( get_template_directory() . '/lib/init.php' );
+include_once(get_template_directory() . '/lib/init.php');
 
-define( 'Child_Theme_Name', __( 'Genesis Child', 'BaseGenesisTheme' ) );
+define('Child_Theme_Name', __('Genesis Child', 'BaseGenesisTheme'));
 
 //* We tell the web address of our child theme (More info & demo)
-define( 'Child_Theme_Url', 'https://github.com/AlexGoiaDev/basegenesistheme.git' );
+define('Child_Theme_Url', 'https://github.com/AlexGoiaDev/basegenesistheme.git');
 //* We tell the version of our child theme
-define( 'Child_Theme_Version', '1.0' );
+define('Child_Theme_Version', '1.0');
 
 //* Add HTML5 markup structure from Genesis
-add_theme_support( 'html5' );
+add_theme_support('html5');
 
 //* Add HTML5 responsive recognition
-add_theme_support( 'genesis-responsive-viewport' );
+add_theme_support('genesis-responsive-viewport');
+
+
+
+
+function be_footer()
+{
+    echo '<div class="left"><p>© Copyright ' . date('Y') . ' : All Rights Reserved</p></div>';
+    echo '<div class="right">';
+    wp_nav_menu(array('menu' => 'Footer'));
+    echo '</div>';
+}
+
+remove_action('genesis_footer', 'genesis_do_footer');
+add_action('genesis_footer', 'be_footer');
